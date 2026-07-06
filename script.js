50aa407aa96a560d0788c79ecd8b006ffe74b554
shaikhaman0120-sys <shaikhaman0120@gmail.com>
2026-07-06 18:53:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
 
+    const button = document.querySelector("button");
+
+    button.addEventListener("mouseenter", () => {
+        button.style.transform = "scale(1.05)";
+    });
+
+    button.addEventListener("mouseleave", () => {
+        button.style.transform = "scale(1)";
+    });
+
+});
